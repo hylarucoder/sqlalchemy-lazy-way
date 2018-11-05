2be8361e46e330750497d25ecaba0c12eca65022
twocucao <twocucao@gmail.com>
2018-11-05 10:30:42 +0800

    initial finished

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ import datetime
 
 import pytest
 
-from tests.models import engine, Base, session
+from tests.models import session, Blog, Entry
 
 
 @pytest.fixture
@@ -18,6 +18,60 @@ def setup_database(request):
     request.addfinalizer(teardown_database)
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def my_session():
     return session
+
+
+@pytest.fixture(scope='session')
+def b2(my_session):
+    b = Blog(
+        name="blog2",
+        entries=[
+            Entry(
+                headline="b2 headline 1",
+                body="body 1",
+                pub_date=datetime.date(2010, 5, 12),
+            ),
+            Entry(
+                headline="b2 headline 2",
+                body="body 2",
+                pub_date=datetime.date(2010, 7, 18),
+            ),
+            Entry(
+                headline="b2 headline 3",
+                body="body 3",
+                pub_date=datetime.date(2011, 8, 27),
+            ),
+        ],
+    )
+    my_session.add(b)
+    my_session.commit()
+    return b
+
+
+@pytest.fixture(scope='session')
+def b1(my_session):
+    b = Blog(
+        name="blog1",
+        entries=[
+            Entry(
+                headline="b1 headline 1",
+                body="body 1",
+                pub_date=datetime.date(2010, 2, 5),
+            ),
+            Entry(
+                headline="b1 headline 2",
+                body="body 2",
+                pub_date=datetime.date(2010, 4, 8),
+            ),
+            Entry(
+                headline="b1 headline 3",
+                body="body 3",
+                pub_date=datetime.date(2010, 9, 14),
+            ),
+        ],
+    )
+    my_session.add(b)
+    my_session.commit()
+    return b
